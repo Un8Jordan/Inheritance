c4a633203529ee9684a151f69722a5a43bbfc553
Un8Jordan <laxmirathore554@gmail.com>
2025-09-24 20:41:42 +0530

    Add files via upload

---
+++ Employee_inheritance.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self,name,id_number):
+        self.name = name
+        self.id_number = id_number 
+
+        def display(self):
+            print(self.name)
+            print(self.id_number)
+
+class Employee(Person):
+    def __init__(self,name,id_number,salary,post):
+        self.salary = salary
+        self.post = post             
+
+        Person.__init__(self,name,id_number)
+
+a = Employee("Anurag",12543,25000,"Intern")
+
+a.display()
